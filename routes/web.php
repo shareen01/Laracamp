72aaba8c6aa22185d6486f3453b20a2fca35f644
shareen <shareenangelica01@gmail.com>
2022-04-09 10:14:21 +0700

    Progress sampai integrasi landing page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('checkout/{camp:slug}', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');
+
+
+Route::get('sign-in-google', [UserController::class,'google'])->name('user.login.google');
+Route::get('auth/google/callback',[UserController::class,'handleProviderCallback'])->name('user.google.callback');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+
+require __DIR__.'/auth.php';
